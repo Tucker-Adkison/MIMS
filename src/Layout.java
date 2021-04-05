b71e378df8be21bd31b5b483314beb083ae6cbba
jlm0135 <jlm0135@auburn.edu>
2021-04-05 16:42:52 -0500

    Added addPrecription and getPrescription methods
    These methods attempt to establish a connection to the database, then will create and send commands to the database for it to execute. Currently missing a url to our database

--- src/Layout.java
+++ src/Layout.java
@@ -15,150 +15,155 @@ import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.*;
 
 public class Layout {
-    private JPanel listPanel;
-    private JTabbedPane tabbed_pane;
-    private JTextField db_username;
-    private JTextField db_password;
-    private String db_password_s;
-    private String db_username_s;
-    private Database db;
+   private JPanel listPanel;
+   private JTabbedPane tabbed_pane;
+   private JTextField db_username;
+   private JTextField db_password;
+   private String db_password_s;
+   private String db_username_s;
+   private Database db;
 
-    public Layout() {
-        listPanel = new JPanel();
-    }
+   public Layout() {
+      listPanel = new JPanel();
+   }
 
-    // create a tabbed pane with two tabs 
-    public void addTabPane() {
-        JFrame frame = MIMS.getFrame();
-        tabbed_pane = new JTabbedPane();
-        frame.add(tabbed_pane);
-
-        JPanel new_panel = newPresciption();  
-        tabbed_pane.addTab("New Prescription", new_panel);
-
-        JPanel list_panel = listPanel;
-        tabbed_pane.addTab("Prescriptions", list_panel);
-
-        tabbed_pane.addChangeListener(new ChangeListener(){
+   // create a tabbed pane with two tabs 
+   public void addTabPane() {
+      JFrame frame = MIMS.getFrame();
+      tabbed_pane = new JTabbedPane();
+      frame.add(tabbed_pane);
+   
+      JPanel new_panel = newPresciption();  
+      tabbed_pane.addTab("New Prescription", new_panel);
+   
+      JPanel list_panel = listPanel;
+      tabbed_pane.addTab("Prescriptions", list_panel);
+   
+      tabbed_pane.addChangeListener(
+         new ChangeListener(){
             public void stateChanged(ChangeEvent e) {
-                if (tabbed_pane.getSelectedIndex() == 1) {
-                    // TODO add a JList to the panel at index 1 
-                    return;
-                }
+               if (tabbed_pane.getSelectedIndex() == 1) {
+               // TODO add a JList to the panel at index 1 
+                  return;
+               }
             }
-            
-        });
-    }
+         
+         });
+   }
 
-    // make a menu pane which adds a "database tab" for logging into the database 
-    public void addMenuPane() {
-        JFrame frame = MIMS.getFrame();
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Settings");
-        menuBar.add(menu);
-        
-        // create the JPanel to add to the "database tab"
-        JPanel settings = new JPanel();
-        settings.setLayout(new BoxLayout(settings, BoxLayout.PAGE_AXIS));
-        
-        JLabel username_l = new JLabel("Username", JLabel.TRAILING);
-        settings.add(username_l);
-        db_username = new JTextField(20);
-        db_username.setMaximumSize(db_username.getPreferredSize());
-        settings.add(db_username);
-
-        JLabel password_l = new JLabel("Password", JLabel.TRAILING);
-        settings.add(password_l);
-        db_password = new JTextField(20);
-        db_password.setMaximumSize(db_password.getPreferredSize());
-        settings.add(db_password);
-
-        JButton submit = new JButton("Submit");
-        // get username and password fields if submit is pressed
-        // delete the database tab from the tabbed pane
-        submit.addActionListener(new ActionListener() {
+   // make a menu pane which adds a "database tab" for logging into the database 
+   public void addMenuPane() {
+      JFrame frame = MIMS.getFrame();
+      JMenuBar menuBar = new JMenuBar();
+      JMenu menu = new JMenu("Settings");
+      menuBar.add(menu);
+      
+      // create the JPanel to add to the "database tab"
+      JPanel settings = new JPanel();
+      settings.setLayout(new BoxLayout(settings, BoxLayout.PAGE_AXIS));
+      
+      JLabel username_l = new JLabel("Username", JLabel.TRAILING);
+      settings.add(username_l);
+      db_username = new JTextField(20);
+      db_username.setMaximumSize(db_username.getPreferredSize());
+      settings.add(db_username);
+   
+      JLabel password_l = new JLabel("Password", JLabel.TRAILING);
+      settings.add(password_l);
+      db_password = new JTextField(20);
+      db_password.setMaximumSize(db_password.getPreferredSize());
+      settings.add(db_password);
+   
+      JButton submit = new JButton("Submit");
+      // get username and password fields if submit is pressed
+      // delete the database tab from the tabbed pane
+      submit.addActionListener(
+         new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                db_username_s = db_username.getText();
-                db_password_s = db_password.getText();
-                // JLabel submitted = new JLabel("Submitted");
-                // settings.add(submitted);
-                tabbed_pane.remove(settings);
+               db_username_s = db_username.getText();
+               db_password_s = db_password.getText();
+            // JLabel submitted = new JLabel("Submitted");
+            // settings.add(submitted);
+               tabbed_pane.remove(settings);
             }
-        });
-
-        settings.add(submit);
-
-        // if the menuitem is pressed, then add database tab
-        menu.addMenuListener(new MenuListener() {
+         });
+   
+      settings.add(submit);
+   
+      // if the menuitem is pressed, then add database tab
+      menu.addMenuListener(
+         new MenuListener() {
             public void menuSelected(MenuEvent e) {
-                tabbed_pane.addTab("Database", settings);
+               tabbed_pane.addTab("Database", settings);
             }
             public void menuDeselected(MenuEvent e) {
-
+            
             }
             public void menuCanceled(MenuEvent e) {
             
             }
+         
+         });
+      frame.setJMenuBar(menuBar);
+      frame.revalidate();
+   }
 
-        });
-        frame.setJMenuBar(menuBar);
-        frame.revalidate();
-    }
-
-    // panel for making a new prescription
-    public JPanel newPresciption() {
-        JPanel p = new JPanel();
-        p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
-
-        // Presciptionts have a name, quantity, and drug 
-        String[] labels = {"First Name: ", "Last Name: ", "Drug: ", "Quantity: "};
-        int num_pairs = labels.length;
-        JTextField[] field_list = new JTextField[num_pairs];
-
-        //populate the panel.
-        for (int i = 0; i < num_pairs; i++) {
-            JLabel l = new JLabel(labels[i], JLabel.TRAILING);
-            p.add(l);
-            JTextField text_field = new JTextField(20);
-            field_list[i] = text_field;
-            text_field.setMaximumSize(text_field.getPreferredSize());
-            l.setLabelFor(text_field);
-            p.add(text_field);
-        }  
-
-        JButton add_button = new JButton("Add Presciption");
-        // when add is pressed, then a new prescription is added to the database
-        add_button.addActionListener(new ActionListener() {
-            
+   // panel for making a new prescription
+   public JPanel newPresciption() {
+      JPanel p = new JPanel();
+      p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
+   
+      // Presciptionts have a name, quantity, and drug 
+      String[] labels = {"First Name: ", "Last Name: ", "Drug: ", "Quantity: "};
+      int num_pairs = labels.length;
+      JTextField[] field_list = new JTextField[num_pairs];
+   
+      //populate the panel.
+      for (int i = 0; i < num_pairs; i++) {
+         JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+         p.add(l);
+         JTextField text_field = new JTextField(20);
+         field_list[i] = text_field;
+         text_field.setMaximumSize(text_field.getPreferredSize());
+         l.setLabelFor(text_field);
+         p.add(text_field);
+      }  
+   
+      JButton add_button = new JButton("Add Presciption");
+      // when add is pressed, then a new prescription is added to the database
+      add_button.addActionListener(
+         new ActionListener() {
+         
             public void actionPerformed(ActionEvent e) {
-                for (int i = 0; i < num_pairs; i++) {
-                    if (field_list[i].getText().equals("")) {
-                        return;
-                    }
-                }
-                String first_name = field_list[0].getText();
-                String last_name = field_list[1].getText();
-                String drug = field_list[2].getText();
-                String quantity = field_list[3].getText();
-                
-                // if the quantity is not an integer then do not set the presciption
-                try {
-                    Integer.parseInt(field_list[3].getText());
-                    //TODO connect to the database and add a presciption to the database
-                    db = new Database("database", db_username_s, db_password_s);
-                } catch (NumberFormatException | SQLException ex){
-                    JLabel err;
-                    if (ex instanceof NumberFormatException)   
-                        err = new JLabel("Could not add to the database");
-                    else 
-                        err = new JLabel("Could connect to the database");
-                }
+               for (int i = 0; i < num_pairs; i++) {
+                  if (field_list[i].getText().equals("")) {
+                     return;
+                  }
+               }
+               String first_name = field_list[0].getText();
+               String last_name = field_list[1].getText();
+               String drug = field_list[2].getText();
+               String quantity = field_list[3].getText();
+            
+            // if the quantity is not an integer then do not set the presciption
+               try {
+                  Integer.parseInt(field_list[3].getText());
+               //TODO connect to the database and add a presciption to the database
+                  db = new Database("database", db_username_s, db_password_s);
+               } catch (NumberFormatException | SQLException ex){
+                  JLabel err;
+                  if (ex instanceof NumberFormatException)   
+                     err = new JLabel("Could not add to the database");
+                  else 
+                     err = new JLabel("Could connect to the database");
+               }
             }
-        });
-
-        p.add(add_button);
-        return p;
-    }
+         });
+   
+      p.add(add_button);
+      return p;
+   }
 }
--- src/MIMS.java
+++ src/MIMS.java
@@ -2,30 +2,30 @@ import javax.swing.JFrame;
 
 class MIMS {
 
-    private static int frame_size = 400;
-    private static JFrame frame;
-    public static void main(String[] args) {
-        makeFrame();
-    }
+   private static int frame_size = 400;
+   private static JFrame frame;
+   public static void main(String[] args) {
+      makeFrame();
+   }
 
-    private static void makeFrame() {
-        frame = new JFrame("Medical Information System");
-        frame.setSize(frame_size, frame_size);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Layout layout = new Layout();
-        layout.addTabPane();
-        layout.addMenuPane();
-    }
+   private static void makeFrame() {
+      frame = new JFrame("Medical Information System");
+      frame.setSize(frame_size, frame_size);
+      frame.setVisible(true);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Layout layout = new Layout();
+      layout.addTabPane();
+      layout.addMenuPane();
+   }
 
-    public static JFrame getFrame() {
-        return frame;
-    }
+   public static JFrame getFrame() {
+      return frame;
+   }
 
-    public static void setFrameSize(int size) {
-        frame.setSize(size, size);
-        frame.revalidate();
-        frame_size = size;
-
-    }
+   public static void setFrameSize(int size) {
+      frame.setSize(size, size);
+      frame.revalidate();
+      frame_size = size;
+   
+   }
 }
