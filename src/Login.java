a5fd3996396e042a8faf26a0a1fdc7e5daec9d5a
Tucker Adkison <tuckeroa@gmail.com>
2021-04-13 11:28:47 -0500

    Changes JTextField to JPasswordField

--- src/Login.java
+++ src/Login.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
@@ -10,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Login {
@@ -26,7 +28,7 @@ public class Login {
         lPanel.setLayout(new BoxLayout(lPanel, BoxLayout.PAGE_AXIS));
 
         JTextField user_field = new JTextField(20);
-        JTextField pass_field = new JTextField(20);
+        JPasswordField pass_field = new JPasswordField(20);
         user_field.setMaximumSize(user_field.getPreferredSize());
         pass_field.setMaximumSize(user_field.getPreferredSize());
         user_field.setText("");
@@ -46,14 +48,15 @@ public class Login {
         submit.addActionListener(new ActionListener() { 
             public void actionPerformed(ActionEvent e) {
                 String username = user_field.getText();
-                String password = pass_field.getText();
+                char[] password = pass_field.getPassword();
 
-                if (username != "" && password != "") {
+                if (username != "" && password.length != 1){
                     try {
                         Scanner scanner = new Scanner(new File("login.txt"));
                         while (scanner.hasNextLine()) {
                             String[] line = scanner.nextLine().split("\\s+");
-                            if (line[0].equals(username) && line[1].equals(password)) {
+                            char[] input_pass = line[1].toCharArray();
+                            if (line[0].equals(username) && Arrays.equals(password, input_pass)) {
                                 frame.remove(lPanel);
                                 layout.addTabPane();
                                 frame.repaint();
